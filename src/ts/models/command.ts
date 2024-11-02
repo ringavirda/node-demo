accf14955de637b32b1ec5c193d1baf63ee0c00f
Fallenstar <ringanaro@gmail.com>
2024-11-02 23:20:27 +0200

    Remove old lines

--- src/ts/models/command.ts
+++ src/ts/models/command.ts
@@ -4,7 +4,3 @@ export interface Command {
   selectors: Array<string>;
   actionAsync: (args: CArgs) => Promise<void>;
 }
-
-export class CommandList {}
-
-export const commands: Array<Command> = [];
